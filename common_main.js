68714a86e558723a96984b9555353047c9880e6b
Victor-python <64462520+Victor-python@users.noreply.github.com>
2022-02-25 15:12:38 +0200

    added main files

---
+++ common_main.js
@@ -0,0 +1,28 @@
+//alert("yo!")
+
+const menuBar = document.querySelector("#menuBar");
+const upButton = document.querySelector("#up");
+const welcome = document.querySelector("#welcome");
+
+window.onscroll = () => scrollFunction()
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        menuBar.classList.add("scrollUp");
+        upButton.classList.remove("invisible");
+        welcome.classList.remove("invisible");
+        
+    } else {  
+        menuBar.classList.remove("scrollUp"); 
+        upButton.classList.add("invisible");    
+    }
+  }
+
+upButton.addEventListener("click", pageTop);
+
+const pageTop = () => {
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
